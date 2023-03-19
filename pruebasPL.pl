76fcae1d529d4b0de1049b99160735d14624414a
RaulMoratilla <79266992+RaulMoratilla@users.noreply.github.com>
2023-03-19 17:48:44 +0100

    Casi Regla2

--- pruebasPL.pl
+++ pruebasPL.pl
@@ -1,5 +1,1 @@
-primeros(L, [], L, 0).
-primeros([X | R], [X], R, 1).
-primeros([X | R], [X | R1], R2, N) :- N1 is N-1, primeros(R, R1, R2, N1).
-
-get(L, INDICE, X) :- INDICE2 is INDICE-1, primeros(L, _, [X | _], INDICE2).
+hola([_|_]).
